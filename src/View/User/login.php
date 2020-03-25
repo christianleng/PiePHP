c41fa2fa861e35592c9cba58577c02b50356c395
christian.leng <christian.leng@epitech.eu>
2020-03-25 20:11:54 +0100

    champs email et password

---
+++ src/View/User/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" methos="post">
+        <div>
+            <label for="email">email :</label>
+            <input type="email" id="email" name="email">
+        </div>
+        <div>
+            <label for="password">password :</label>
+            <input type="password" id="password" name="password">
+        </div>
+        <div>
+            <label for="submit">submit :</label>
+            <input type="submit" id="submit" name="submit">
+        </div>
+    </form>
+</body>
+</html>
