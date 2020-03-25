4b159b77fb5c8b067eb95ebd8b8b4a5faf8a47ea
christian.leng <christian.leng@epitech.eu>
2020-03-25 23:37:52 +0100

    autoload pour execute une instance

--- Core/autoload.php
+++ Core/autoload.php
@@ -1,6 +1,7 @@
 <?php
-
+// function callback quand il veut chercher une instance
 function chargerUneClasse($classe) {
     require 'Core' . '.php'; 
 }
+
 spl_autoload_register('chargerUneClasse'); 
