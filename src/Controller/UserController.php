332f1b726056efff7e8e0ab5a9b6bc9a43960e51
christian.leng <christian.leng@epitech.eu>
2020-03-26 23:11:33 +0100

    mise en place de la method appAction et indexAction

---
+++ src/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controller;
+
+class UserController {
+
+    public function appAction() {
+        echo "UserController/addAction OK OK";
+    }
+ 
+    public function indexAction () {
+        echo "UserController/indexAction OK OK";
+    }
+
+}
