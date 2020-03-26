5146476c1656f69a4146a97aed2f96f2cdba5643
christian.leng <christian.leng@epitech.eu>
2020-03-26 23:10:22 +0100

    Je verifie si le controller ou l'action fournie n'existe pas alors message 404 sinon je l'instancie

--- Core/Core.php
+++ Core/Core.php
@@ -6,5 +6,27 @@ class Core {
 
     public function run(){
         echo __CLASS__ . " [OK] " . PHP_EOL;
+        echo $_SERVER['REQUEST_URI'] . '<br>';
+
+        $arr = explode("/", $_SERVER['REQUEST_URI']);
+        print_r($arr);
+        echo  '<br>';
+        if (isset($arr[2]) && isset($arr[3])) {
+            $classe = ucfirst($arr[2]) . "Controller";
+            $method = $arr[3] . "Action";
+            echo "$classe -> $method<br>";
+            if (class_exists($class())) {
+                $controller = new $class();
+                if (method_exists($method)) {
+                    $controller->$method();
+                }else {
+                    echo "fail method";
+                }
+            }else {
+                echo "fail class";
+            }
+        }else {
+            echo "404";
+        }
     }
 }
