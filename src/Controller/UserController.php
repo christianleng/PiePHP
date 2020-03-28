db2c972a3492189c6cc84a9bd183e78efaad16a0
christian.leng <christian.leng@epitech.eu>
2020-03-28 02:36:23 +0100

    UserController php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,15 +1,17 @@
 <?php
-
 namespace Controller;
 
-class UserController {
+class UserController  extends \Core\Controller{
 
     public function appAction() {
-        echo "UserController/addAction OK OK";
+        echo "UserController/addAction OK OK<br>";
     }
  
     public function indexAction () {
-        echo "UserController/indexAction OK OK";
+        echo "UserController/indexAction OK OK<br>";
     }
 
+    public function registerAction() {
+        echo "UserController/registerAction OK OK<br>";
+    }
 }
