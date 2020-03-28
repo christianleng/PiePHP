b8b4f15a4c942bd2bc35260f1662071ed16d9ac3
christian.leng <christian.leng@epitech.eu>
2020-03-28 02:36:06 +0100

    AppController php

---
+++ src/Controller/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class AppController extends \Core\Controller{
+
+    public function indexAction () {
+        echo "AppController/indexAction<br>";
+    }
+
+    public function aboutUsAction() {
+        echo "AppController/aboutUsAction<br>";
+    }
+
+}
