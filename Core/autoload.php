58592e789a1f3c9f6bcfba13fe10a3a5ae9e9237
christian.leng <christian.leng@epitech.eu>
2020-03-30 22:24:17 +0200

    autoload dynamique qui prend en compte aussi les dossiers Controller Model et View dans le src

--- Core/autoload.php
+++ Core/autoload.php
@@ -1,16 +1,19 @@
 <?php
 
 function addAllClass ($class) {
-
-    if (explode('/', $class) == 'Core') {
-        if (file_exists('/var/www/html/MVC_PiePHP/' . $class . '.php')) {
-            require_once('/var/www/html/MVC_PiePHP/' . $class . '.php');
+    $srcFolders = ['Controller', 'Model', 'View'];
+    $nameSpace = explode("\\", $class)[0]; //Core
+    $className = explode("\\", $class)[1]; // les autres
+    // echo $nameSpace . ' test<br>';
+    if ($nameSpace == 'Core') {
+        if (file_exists('/var/www/html/MVC_PiePHP/' . $nameSpace . '/'. $className . '.php')) {
+            require_once('/var/www/html/MVC_PiePHP/' . $nameSpace . '/'. $className . '.php');
         }
-    } elseif (in_array(explode('/', $class) , ['Model', 'View', 'Controller'])) {
-        if (file_exists('/var/www/html/MVC_PiePHP/' . $class . '.php')) {
-            require_once('/var/www/html/MVC_PiePHP/' . $class . '.php');
+    } 
+    elseif(in_array($nameSpace, $srcFolders)) {
+        if (file_exists('/var/www/html/MVC_PiePHP/src/' . $nameSpace . '/' . $className . '.php')) {
+            require_once('/var/www/html/MVC_PiePHP/src/' . $nameSpace . '/' . $className . '.php');
         }
     }
 }
-
 spl_autoload_register("addAllClass");
