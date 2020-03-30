cacf0dcfc39ccd4ed311b826ebc93401cd042074
christian.leng <christian.leng@epitech.eu>
2020-03-30 22:26:08 +0200

    le Core avec le router static

--- Core/Core.php
+++ Core/Core.php
@@ -1,33 +1,47 @@
 <?php
-
 namespace Core;
 
 class Core {
 
-    public function run(){
-        echo __CLASS__ . " [OK] " . PHP_EOL;
-        echo $_SERVER['REQUEST_URI'] . '<br>';
+    public function __construct() {
+        require_once("src/routes.php");
+    }
 
-        $arr = explode("/", $_SERVER['REQUEST_URI']);
+    public function run(){
+        echo __CLASS__ . " [OK] " . '<br>';
+        echo $_SERVER['REQUEST_URI'] . '<br>';
+    
+        $arr = explode('/' , $_SERVER['REQUEST_URI']);
         print_r($arr);
         echo  '<br>';
-        if (isset($arr[2]) && isset($arr[3])) {
-            $classe = ucfirst($arr[2]) . "Controller";
-            $method = $arr[3] . "Action";
-            echo "$classe -> $method<br>";
-            if (class_exists($class())) {
-                $controller = new $class();
-                if (method_exists($method)) {
-                    $controller->$method();
+        echo $_SERVER['REQUEST_URI'] . '<br>';
+
+        if(Router::get($_SERVER['REQUEST_URI']) != null) {
+
+            echo "Custom route found<br>";
+            echo $control;
+            
+        }else {   
+            // SOIT ROUTER DYNAMIQUE(les routes dynamique devront finir par action)
+            if (isset($arr[2]) && isset($arr[3])) {
+                $classe = 'Controller\\' . ucfirst($arr[2]) . "Controller";
+                $method = $arr[3] . "Action";
+                echo "$classe -> $method<br>";
+                if (class_exists($classe)) {
+                    if (method_exists($classe, $method)) {
+                        $controller = new $classe();
+                        $controller->$method();
+                    }else {
+                        echo "fail method";
+                    }
                 }else {
-                    echo "fail method";
+                    echo "fail class";
                 }
             }else {
-                echo "fail class";
+                if (!class_exists($classe) && !method_exists($method)) {
+                    echo "404";
+                }
             }
-        }else {
-            echo "404";
         }
     }
-    
 }
