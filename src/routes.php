111ea2567374ff208e76c84a6cb5fd455ce1c4f3
christian.leng <christian.leng@epitech.eu>
2020-03-30 22:36:02 +0200

    le routes avec les parametes url et route pour le router static

---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Core\Router::connect('/', ['controller' => 'app','action' => 'index']);
+Core\Router::connect('/register', ['controller' => 'user','action' => 'add']);
+Core\Router::connect('/MVC_PiePHP/user/salut', ['controller' => 'user','action' => 'salut']);
