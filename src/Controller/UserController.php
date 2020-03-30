f29f411e9a51c17bcbcf2bf60970102052c7ef06
christian.leng <christian.leng@epitech.eu>
2020-03-30 22:31:19 +0200

    le UserController avec un petit debut de render mais qui ne marche pas

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,17 +1,27 @@
 <?php
 namespace Controller;
 
-class UserController  extends \Core\Controller{
+class UserController extends \Core\Controller{
 
     public function appAction() {
-        echo "UserController/addAction OK OK<br>";
-    }
- 
-    public function indexAction () {
-        echo "UserController/indexAction OK OK<br>";
+        echo "UserController/addAction [OK]";
+
     }
 
+    public function indexAction () {
+        echo "UserController/indexAction [OK]";
+        $users = UserModel::readAll();
+        echo self::render("index",[$users]);
+    }
+
+
     public function registerAction() {
-        echo "UserController/registerAction OK OK<br>";
+        echo "UserController/registerAction [OK]";
+        $users = UserModel::readAll();
+        echo self::render("register", [$users]);
+    }
+
+    public function salutAction () {
+        echo "j'ai reussi la route static [OK]";
     }
 }
