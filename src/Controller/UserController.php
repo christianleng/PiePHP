5490dbfc2915277b45ff2721d8eff33d70223924
christian.leng <christian.leng@epitech.eu>
2020-03-31 23:13:47 +0200

    J'ai juste ajouter les method salut et bonjour pour le router static et les method les les View

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,23 +5,31 @@ class UserController extends \Core\Controller{
 
     public function appAction() {
         echo "UserController/addAction [OK]";
-
-    }
-
-    public function indexAction () {
-        echo "UserController/indexAction [OK]";
-        $users = UserModel::readAll();
-        echo self::render("index",[$users]);
-    }
-
-
-    public function registerAction() {
-        echo "UserController/registerAction [OK]";
-        $users = UserModel::readAll();
-        echo self::render("register", [$users]);
     }
 
     public function salutAction () {
-        echo "j'ai reussi la route static [OK]";
+        echo "j'ai reussi la route static [OK] / salutAction";
     }
+    public function bonjourAction () {
+        echo "j'ai reussi la route static [OK] / bonjourAction";
+    }
+    public function indexAction () {
+        echo "UserController/indexAction [OK]";
+        self::render("index");
+    }
+
+    public function registerAction() {
+        echo "UserController/registerAction [OK]";
+        echo self::render("register");
+    }
+
+    public function loginAction () {
+        echo "UserController/loginAction [OK]";
+        echo self::render("login");
+    } 
+    public function showAction() {
+        echo "UserController/showAction [OK]";
+        echo self::render("show");
+    }
+
 }
