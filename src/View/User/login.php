f2906b216d97619fef727b5a60beeebfd78cb956
christian.leng <christian.leng@epitech.eu>
2020-03-31 23:16:42 +0200

    login php

--- src/View/User/login.php
+++ src/View/User/login.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" methos="post">
-        <div>
-            <label for="email">email :</label>
-            <input type="email" id="email" name="email">
-        </div>
-        <div>
-            <label for="password">password :</label>
-            <input type="password" id="password" name="password">
-        </div>
-        <div>
-            <label for="submit">submit :</label>
-            <input type="submit" id="submit" name="submit">
-        </div>
-    </form>
-</body>
-</html>
+<h2>Vos login :</h2>
+<form action="/user/login" method="post">
+        <input type="email" id="email" name="email" placeholder="Votre adresse mail">
+        <input type="password" id="password" name="password" placeholder="Votre mot de passe">
+        <input type="submit" id="submit" name="submit">
+    </div>
+</form>
