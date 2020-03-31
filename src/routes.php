31779bcc26685d817482ea64b4828bf69945c5bf
christian.leng <christian.leng@epitech.eu>
2020-03-31 23:15:41 +0200

    route en plus

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,6 @@
 
 Core\Router::connect('/', ['controller' => 'app','action' => 'index']);
 Core\Router::connect('/register', ['controller' => 'user','action' => 'add']);
-Core\Router::connect('/MVC_PiePHP/user/salut', ['controller' => 'user','action' => 'salut']);
+Core\Router::connect('/MVC_PiePHP/user/salut', ['controller' => 'user','action' => 'filter']);
+Core\Router::connect('/MVC_PiePHP/user/bonjour', ['controller' => 'user','action' => 'filter']);
+echo "bonjour du routes.php" . '<br>';
