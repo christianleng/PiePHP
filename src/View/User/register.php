1e1467b1295048feeda58135d4cde39db76f9944
christian.leng <christian.leng@epitech.eu>
2020-03-31 23:17:01 +0200

    register php

---
+++ src/View/User/register.php
@@ -0,0 +1,10 @@
+<h2>Register</h2>
+<a href="/user/register">Register DEUX</a>
+<form action="/user/register" method="post">
+        <input type="email" id="email" name="email" placeholder="Votre adresse mail">
+        <input type="email" id="email2" name="email" placeholder="Valider votre adresse mail">
+        <input type="password" id="password" name="password" placeholder="Votre mot de passe">
+        <input type="password" id="password2" name="password" placeholder="Valider votre mot de passe">
+        <input type="submit" id="submit" name="submit">
+    </div>
+</form>
