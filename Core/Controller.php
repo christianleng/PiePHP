213801314d8bf901310283080358a4860ce29639
christian.leng <christian.leng@epitech.eu>
2020-03-31 23:10:21 +0200

    Controller qui marche avec le bon chemin et qui va bien dans le index login register et show

--- Core/Controller.php
+++ Core/Controller.php
@@ -7,15 +7,27 @@ class Controller {
 
     protected function render ($view, $scope = []) {
         extract($scope);
-        $template = implode (DIRECTORY_SEPARATOR,[dirname(__DIR__), 'src', 'View', str_replace ('Controller', '', basename(get_class($this))), $view]) . '.php';
+
+        $a = array('Controller\\', 'Controller');
+        $b = "";
+        
+        $template = trim(implode (DIRECTORY_SEPARATOR,[dirname(__DIR__), 'src', 'View', str_replace($a,$b,basename(get_class($this))), $view])) . '.php';
         if (file_exists($template)) {
             ob_start();
-            include ($template);
+            require_once ($template);
             $view = ob_get_clean();
             ob_start () ;
-            include (implode(DIRECTORY_SEPARATOR,[dirname(__DIR__), 'src', 'View', 'index']) . '. php ') ;
+            require_once (implode(DIRECTORY_SEPARATOR,[dirname(__DIR__), 'src', 'View', 'index.php'])) ;
             self::$_render = ob_get_clean();
+        } else{
+            echo "file not exist" . '<br>';
+            echo $template. '<br>';
+            echo __DIR__;
         }
     }
-    
+
+    public function __destruct() {
+        echo self::$_render;
+    }
+
 }
