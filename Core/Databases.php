ae7349d8998fbb084bc2e5fc69dfa9896e68ff6f
christian.leng <christian.leng@epitech.eu>
2020-04-01 21:00:55 +0200

    Base de donnee que je peux recuperer en static OK

--- Core/Databases.php
+++ Core/Databases.php
@@ -1,11 +1,28 @@
 <?php
+namespace core;
 
+class Database {
 
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=MVC_PiePHP;charset=utf8', 'root', 'root',
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    private static $_pdo;
+    private static $_host;
+    private static $_dbName;
+    private static $_username;
+    private static $_password;
+
+    public function __construct() {
+        
+        self::$_host = "127.0.0.1"; 
+        self::$_dbName = "MVC_PiePHP"; 
+        self::$_username = "root"; 
+        self::$_password = "root"; 
+    }
+    
+    public static function getDb () {
+        if (Database::getDb() == null) {
+            self::$pdo = new PDO('mysql:host="'.self::$_host.'";dbname="'.self::$_dbName.'";charset=utf8', '"'.self::$_username.'"', '"'.self::$_password.'"',
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        return self::$_pdo;
+    }
+
 }
-catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-?>
