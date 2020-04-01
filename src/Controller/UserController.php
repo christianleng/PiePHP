16e77c7c6c8eee4fe8b2c41667f92c6003074154
christian.leng <christian.leng@epitech.eu>
2020-04-01 21:04:06 +0200

    public function registerAction pour envoyer ma variable POST dans la request

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,18 +15,36 @@ class UserController extends \Core\Controller{
     }
     public function indexAction () {
         echo "UserController/indexAction [OK]";
-        self::render("index");
+        // self::render("index");
+        // $user = new UserModel(["name" => "", "password" => ""]);
+        // $user->role = "Membre";
+        // $user->registerDate = Date();
+        // $user->create();
     }
-
-    public function registerAction() {
-        echo "UserController/registerAction [OK]";
+    
+    
+    public function viewregisterAction() {
         echo self::render("register");
+    }
+    
+
+    
+    public function registerAction() {
+        // echo "UserController/registerAction [OK]";
+        // echo self::render("register");
+        echo '<pre>', var_dump($_POST), '</pre>';
+        echo '<pre>', var_dump($post), '</pre>';
+        $postsecurise = Request::security($_POST);       //securite des donne
+        $obj = \Model\usermodel($post);                  //instencier les model en lui donnant les donne
+        $obj->save();                                    //appeller la function du model  
+        
     }
 
     public function loginAction () {
         echo "UserController/loginAction [OK]";
         echo self::render("login");
     } 
+
     public function showAction() {
         echo "UserController/showAction [OK]";
         echo self::render("show");
