9448e0771d8f85b959a31ce9a598468dfa7cd811
christian.leng <christian.leng@epitech.eu>
2020-04-05 23:12:07 +0200

    un debut de Entity avec quelque modification a faire

--- Core/Entity.php
+++ Core/Entity.php
@@ -2,21 +2,46 @@
 
 namespace Core;
 
-abstract class Entity {
-// Une classe abstract est une classe qui ne peut pas etre instanciee 
-    private $table;
-    public $values;
-    public $params;
+class Entity {
 
-    public function __construct($params, $values = []) {
-        $class = get_class($this);
+    protected $params;
+    public $tables;
+
+    public function __construct($params) {
+
+        $tables = str_replace('model', '', trim(stripslashes(strlolower(get_class($this)))));
+        
         if (array_key_exists('id', $params) && count($params > 0)) {
-            ORM::read($this->table, $params['id']);
+            $val = ORM::read($this->table, $params['id']);
         } else {
-            ORM::read($params);
+            $val = $params;
         }
+
     }
 
+    public function getAllAttributes($params) {
+        foreach ($params as $key -> $val) {
+            $this->$key = $val;
+        }
+    }
+    public function create() {
+        return ORM::create($tables, $this->relation);
+    }
+    
+    public function read($id) {
+        return ORM::read($tables, $id, $this->relation);
+    }
+
+    public function update($id) {
+        return ORM::update($tables, $id ,$this->relation);
+    }
+
+    public function delete ($id) {
+        return ORM::delete($tables, $id ,$this->relation);
+    }
+
+    // public function find ($id) {
+    //     return ORM::find($tables, $id ,$this->relation);
+    // }
 }
-
-
+ 
