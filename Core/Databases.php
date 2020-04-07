fcbf8fdf0637553c150a5628e63b727bc6edf484
christian.leng <christian.leng@epitech.eu>
2020-04-08 01:18:24 +0200

    Database OK

--- Core/Databases.php
+++ Core/Databases.php
@@ -1,27 +1,27 @@
 <?php
 namespace Core;
+use \PDO;
 
-class Databases {
+class Databases{
 
-    private static $_pdo;
+    public static $_pdo;
     private static $_host;
     private static $_dbName;
     private static $_username;
     private static $_password;
 
     public function __construct() {
-        
+
         self::$_host = "127.0.0.1"; 
         self::$_dbName = "MVC_PiePHP"; 
         self::$_username = "root"; 
         self::$_password = "root"; 
+
     }
     
     public static function getDb () {
-        if (Databases::getDb() == null) {
-            self::$_pdo = new PDO('mysql:host="'.self::$_host.'";dbname="'.self::$_dbName.'";charset=utf8', '"'.self::$_username.'"', '"'.self::$_password.'"',
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }
+        self::$_pdo = new PDO('mysql:host="'.self::$_host.'";dbname="'.self::$_dbName.'";charset=utf8'. '"'.self::$_username.'"'. '"'.self::$_password.'"'.
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return self::$_pdo;
     }
 
