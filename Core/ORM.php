1b5a1e0be0b159b2509550344f4447cc00758d0b
christian.leng <christian.leng@epitech.eu>
2020-04-08 01:21:43 +0200

    quelque modification

--- Core/ORM.php
+++ Core/ORM.php
@@ -5,55 +5,70 @@ class ORM{
 
     public $email;
     public $password;
-    public $_tables;
+    public $tables;
+    public $table;
 
-
-    public function __construct() {    
+    public function __construct() {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+    }
+    /*
+    foreach($params as $key => $value) {
+        echo '<br>' . "La valeur :" . $value . '<br>';
+        // echo "----------------";
+        echo '<br>' . "La Key :" . $key . '<br>'; 
     }
 
-    public function create($_tables, $params) {
-        $db = \Core\Database::getDb(self::$_pdo);
-        $requete = $db->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
-        $requete->execute(array($email, $password));
+    echo '<pre>', var_dump($key), '</pre>';
+    echo '<pre>', var_dump($value), '</pre>';
+    echo '<pre>', var_dump($params), '</pre>';
+    */
+    public static function create($table) {
+        echo "bonjour";
+        echo '<pre>', var_dump($table), '</pre>' . '<br>';
+        echo '<pre>', var_dump($params), '</pre>';
+        $sql = Databases::getDb()->prepare("INSERT INTO $table (email, password) VALUES (:mail , :pwd)");
+        $sql->execute(array(':mail' => $email, ':pwd' => $password));
 
-        // $reqMail = self::$_pdo->prepare('SELECT * FROM $tables WHERE email = ?');
+        // $reqMail = Databases::getDb()->prepare('SELECT * FROM $tables WHERE email = ?');
         // $reqMail->execute(array($email));
+        // $reqUserName = Databases::getDb();
         // $rowCountMail = $reqMail->rowCount();
         // if($rowCountMail == 0){
-        //     $reqUserName = self::$_pdo->prepare('SELECT * FROM $tables WHERE username = ?');
+        //     $reqUserName = Databases::getDb()->prepare('SELECT * FROM $tables WHERE username = ?');
         //     $reqUserName->execute(array($userName));
         //     $rowCountUserName = $reqUserName->rowCount();
         //     if($rowCountUserName == 0){
-        //         $requete = self::$_pdo->query("INSERT INTO $tables (email, password) VALUE(?, ?);");
+        //         $requete = Databases::getDb()->prepare("INSERT INTO $tables (email, password) VALUE(?, ?);");
         //         $requete->execute(array($email, $password));
         //     }
         //     else{
         //         echo 'Fail INSERT';
-        //     }
+        //     };
         // }
         // else{
         //     echo 'fail SELECT';
         // }
     }
 
-    public function read($table, $id) {
-        $readAccount =  self::$_pdo->prepare("SELECT * FROM $table WHERE id = $id");
+    public static function read($table, $id) {
+        $readAccount =  Databases::getDb()->prepare("SELECT * FROM $table WHERE id = $id");
         $readAccount->execute();
         $array = $readAccount->fetch(PDO::FETCH_ASSOC);
         return $array; 
     }
 
-    public function update($table, $id, $fields) {
-        $updateAccount = self::$_pdo->prepare("UPDATE $tables SET email = ?, password = ? WHERE email = ?");
+    public static function update($table, $id, $params) {
+        $updateAccount = Databases::getDb()->prepare("UPDATE $table SET email = ?, password = ? WHERE id = $id");
         $updateAccount->execute(array($email, $password));
     }
 
-    public function delete($table, $id) {
-        $deleteAccount = self::$_pdo->prepare("DELETE FROM $tables WHERE email = ?");
+    public static function delete($table, $id) {
+        $deleteAccount = Databases::getDb()->prepare("DELETE FROM $table WHERE id = $id");
         $deleteAccount->execute();
     }
-    public function connect () {
-        $connectAccount = self::$_pdo->prepare("SELECT * FROM $tables WHERE email = ? AND password = ?");
+    public static function connect ($table) {
+        $connectAccount = Databases::getDb()->prepare("SELECT * FROM $table WHERE email = ? AND password = ?");
         $connectAccount->execute(array($email, $password));
         $accountExist = $connectAccount->rowCount();
         if($accountExist == 1){
