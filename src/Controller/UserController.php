f317286f6ed04ac41212969e747efd3cebe8fc6c
christian.leng <christian.leng@epitech.eu>
2020-04-09 21:24:24 +0200

    un UserController correct qui recoit bien les info

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,10 @@
 namespace Controller;
 
 class UserController extends \Core\Controller{
+
+    public static $_table;
+    public static $attribut;
+
     
     public function viewregisterAction() {
         echo self::render("register");
@@ -10,7 +14,7 @@ class UserController extends \Core\Controller{
     public function viewindexAction() {
         echo self::render("index");
  
-    } 
+    }
     public function viewloginAction() {
         echo self::render("login");
      
@@ -19,24 +23,28 @@ class UserController extends \Core\Controller{
         echo self::render("show");
      
     }
- 
+    public function deleteAction() {
 
+    }
+    public function detailAction() {
+        
+    }
     public function registerAction() {
-        $params = \Core\Request::security($_POST);
-        echo '<pre>', var_dump($params), '</pre>';
 
-        $user = \Model\UserModel::save($params) ;
+        $params = \Core\Request::security($_POST);
+
+        $user = new \Model\UserModel($params, $tableau = [], self::$_table);
+
         if (!$user->id)  {       
-            echo "[OK] DANS LE IF" . '<br>';
-            $user->save();
+            echo "[OK] dans le if de UserController" . '<br>';
+            $user->save($params, $tableau = [], self::$_table);
             self::$_render = "Votre compte a ete cree." . PHP_EOL ;
         }   
 
     }
     public function indexAction () {
-    
-    }
 
+    }
     public function loginAction () {
 
     } 
@@ -46,5 +54,3 @@ class UserController extends \Core\Controller{
     }
 
 }
-
-
