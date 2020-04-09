ef19fb9527ea214acdd9103653b9b464baf97b6c
christian.leng <christian.leng@epitech.eu>
2020-04-09 21:23:40 +0200

    juste des modification de nom de variable

--- Core/ORM.php
+++ Core/ORM.php
@@ -5,70 +5,37 @@ class ORM{
 
     public $email;
     public $password;
-    public $tables;
-    public $table;
+    public static $_table;
+    public static $attribut;
 
-    public function __construct() {
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-    }
-    /*
-    foreach($params as $key => $value) {
-        echo '<br>' . "La valeur :" . $value . '<br>';
-        // echo "----------------";
-        echo '<br>' . "La Key :" . $key . '<br>'; 
-    }
 
-    echo '<pre>', var_dump($key), '</pre>';
-    echo '<pre>', var_dump($value), '</pre>';
-    echo '<pre>', var_dump($params), '</pre>';
-    */
-    public static function create($table) {
-        echo "bonjour";
-        echo '<pre>', var_dump($table), '</pre>' . '<br>';
-        echo '<pre>', var_dump($params), '</pre>';
-        $sql = Databases::getDb()->prepare("INSERT INTO $table (email, password) VALUES (:mail , :pwd)");
-        $sql->execute(array(':mail' => $email, ':pwd' => $password));
+    public static function create($_table, $attribut) {
+        echo "[OK] dans la function create de ORM.php<br>";
+        echo '<pre style="color: red;">', var_dump(self::$_table), '</pre>' . '<br>';
+        echo '<pre style="color: blue;">', var_dump(self::$attribut), '</pre>';
+        $sql = Databases::getDb()->prepare("INSERT INTO self::$_table (email, password) VALUES (:mail , :pwd)");
+        $sql->execute(array(':mail' => $_POST["email"], ':pwd' => $$_POST["password"]));
 
-        // $reqMail = Databases::getDb()->prepare('SELECT * FROM $tables WHERE email = ?');
-        // $reqMail->execute(array($email));
-        // $reqUserName = Databases::getDb();
-        // $rowCountMail = $reqMail->rowCount();
-        // if($rowCountMail == 0){
-        //     $reqUserName = Databases::getDb()->prepare('SELECT * FROM $tables WHERE username = ?');
-        //     $reqUserName->execute(array($userName));
-        //     $rowCountUserName = $reqUserName->rowCount();
-        //     if($rowCountUserName == 0){
-        //         $requete = Databases::getDb()->prepare("INSERT INTO $tables (email, password) VALUE(?, ?);");
-        //         $requete->execute(array($email, $password));
-        //     }
-        //     else{
-        //         echo 'Fail INSERT';
-        //     };
-        // }
-        // else{
-        //     echo 'fail SELECT';
-        // }
     }
 
-    public static function read($table, $id) {
-        $readAccount =  Databases::getDb()->prepare("SELECT * FROM $table WHERE id = $id");
+    public static function read($_table, $id) {
+        $readAccount =  self::$_pdo->prepare("SELECT * FROM $_table WHERE id = $id");
         $readAccount->execute();
         $array = $readAccount->fetch(PDO::FETCH_ASSOC);
         return $array; 
     }
 
-    public static function update($table, $id, $params) {
-        $updateAccount = Databases::getDb()->prepare("UPDATE $table SET email = ?, password = ? WHERE id = $id");
+    public static function update($_table, $id, $params) {
+        $updateAccount = self::$_pdo->prepare("UPDATE $_table SET email = ?, password = ? WHERE id = $id");
         $updateAccount->execute(array($email, $password));
     }
 
-    public static function delete($table, $id) {
-        $deleteAccount = Databases::getDb()->prepare("DELETE FROM $table WHERE id = $id");
+    public static function delete($_table, $id) {
+        $deleteAccount = self::$_pdo->prepare("DELETE FROM $_table WHERE id = $id");
         $deleteAccount->execute();
     }
-    public static function connect ($table) {
-        $connectAccount = Databases::getDb()->prepare("SELECT * FROM $table WHERE email = ? AND password = ?");
+    public static function connect ($_table) {
+        $connectAccount = self::$_pdo->prepare("SELECT * FROM $_table WHERE email = ? AND password = ?");
         $connectAccount->execute(array($email, $password));
         $accountExist = $connectAccount->rowCount();
         if($accountExist == 1){
@@ -80,4 +47,5 @@ class ORM{
             echo "fail CONNECT";
         }
     }
+
 }
