a9a8dc54f7df9a4e87a0eb025d30a0617d076da4
christian.leng <christian.leng@epitech.eu>
2020-04-09 21:25:02 +0200

    pareil, des modifications de nom de variable

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -3,20 +3,18 @@ namespace Model;
 
 class UserModel extends \Core\Entity{
 
-    public $email;
-    public $password;
+    // public $email;
+    // public $password;
+    public static $_table;
+    public static $attribut;
 
-    public function save() {
 
-        echo 'public function save() [OK] <br>';
-        echo '<pre>', var_dump($params), '</pre><br>';
-        echo 'public function save() [OK] <br>';
+    public static function save($params) {
+        echo "save de UserModel[OK]<br>";
+        echo '<pre style="color: green;">', var_dump(self::$attribut), '</pre>';
+        echo '<pre style="color: violet;">', var_dump(self::$_table), '</pre>';
+        return \Core\ORM::create(self::$_table, self::$attribut);
 
-        print_r($params) . '<br>';
-
-        \Core\ORM::create($table, $params);
-        
     }
  
 }
-
