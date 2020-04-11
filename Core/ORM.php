9357f1462393b9b46cd273e2653916ee62ced8a2
christian.leng <christian.leng@epitech.eu>
2020-04-11 02:04:26 +0200

    ORM OK, il manque juste a faire les autres class en dynamique

--- Core/ORM.php
+++ Core/ORM.php
@@ -3,39 +3,40 @@ namespace Core;
 
 class ORM{
 
-    public $email;
-    public $password;
-    public static $_table;
-    public static $attribut;
+    public static $_pdo;
 
+    
+    public static function create($_table, $attribut, $params) {
 
-    public static function create($_table, $attribut) {
-        echo "[OK] dans la function create de ORM.php<br>";
-        echo '<pre style="color: red;">', var_dump(self::$_table), '</pre>' . '<br>';
-        echo '<pre style="color: blue;">', var_dump(self::$attribut), '</pre>';
-        $sql = Databases::getDb()->prepare("INSERT INTO self::$_table (email, password) VALUES (:mail , :pwd)");
-        $sql->execute(array(':mail' => $_POST["email"], ':pwd' => $$_POST["password"]));
+        $maKey = array_keys($params);
+        $mesValues = array_values($params);
+        
+        $mesValuesDeux = str_replace(', Valider', '',  implode(', ', $mesValues));
+        $laSecurite = str_replace($mesValues, '? ', $mesValuesDeux);
+
+        $sql = Databases::getDb()->prepare('INSERT INTO '.$_table.' ('.str_replace(', submit', '',  implode(', ', $maKey)).') VALUES ('.$laSecurite.')');
+        $sql->execute(array($mesValuesDeux));
 
     }
 
     public static function read($_table, $id) {
-        $readAccount =  self::$_pdo->prepare("SELECT * FROM $_table WHERE id = $id");
+        $readAccount =  $this->_pdo->prepare("SELECT * FROM $_table WHERE id = $id");
         $readAccount->execute();
         $array = $readAccount->fetch(PDO::FETCH_ASSOC);
         return $array; 
     }
 
-    public static function update($_table, $id, $params) {
-        $updateAccount = self::$_pdo->prepare("UPDATE $_table SET email = ?, password = ? WHERE id = $id");
+    public static function update($_table, $attribut, $params) {
+        $updateAccount = $this->_pdo->prepare("UPDATE $_table SET email = ?, password = ? WHERE id = $id");
         $updateAccount->execute(array($email, $password));
     }
 
     public static function delete($_table, $id) {
-        $deleteAccount = self::$_pdo->prepare("DELETE FROM $_table WHERE id = $id");
+        $deleteAccount = $this->_pdo->prepare("DELETE FROM $_table WHERE id = $id");
         $deleteAccount->execute();
     }
-    public static function connect ($_table) {
-        $connectAccount = self::$_pdo->prepare("SELECT * FROM $_table WHERE email = ? AND password = ?");
+    public static function connect ($_table, $attribut, $params) {
+        $connectAccount = $this->_pdo->prepare("SELECT * FROM $_table WHERE email = ? AND password = ?");
         $connectAccount->execute(array($email, $password));
         $accountExist = $connectAccount->rowCount();
         if($accountExist == 1){
