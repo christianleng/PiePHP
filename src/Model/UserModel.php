3c1d398846aacc6f41419e49e06cafdd92313bef
christian.leng <christian.leng@epitech.eu>
2020-04-11 02:05:56 +0200

    UserModel OK pour le create

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -3,18 +3,11 @@ namespace Model;
 
 class UserModel extends \Core\Entity{
 
-    // public $email;
-    // public $password;
-    public static $_table;
-    public static $attribut;
+    public $_table;
+    public $attribut;
 
-
-    public static function save($params) {
-        echo "save de UserModel[OK]<br>";
-        echo '<pre style="color: green;">', var_dump(self::$attribut), '</pre>';
-        echo '<pre style="color: violet;">', var_dump(self::$_table), '</pre>';
-        return \Core\ORM::create(self::$_table, self::$attribut);
-
+    public function save($params) {
+        return \Core\ORM::create($this->_table, $this->attribut, $params);
     }
- 
+
 }
