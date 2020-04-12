0e2626df0f7d22c7a93312d654d8ed3482b9825c
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:42:39 +0200

    ok

--- src/View/User/login.php
+++ src/View/User/login.php
@@ -1,5 +1,6 @@
-<h2>Vos login :</h2>
-<form action="/user/login" method="post">
+<h2>Veuillez entrer votre email et un mot de passe.</h2>
+<form action="/MVC_PiePHP/user/login" method="post">
+    <div>
         <input type="email" id="email" name="email" placeholder="Votre adresse mail">
         <input type="password" id="password" name="password" placeholder="Votre mot de passe">
         <input type="submit" id="submit" name="submit">
