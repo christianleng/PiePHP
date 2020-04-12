782cee852572214022ba2aafc1015b7ec6fa5876
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:41:36 +0200

    OK

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,44 +2,42 @@
 namespace Controller;
 
 class UserController extends \Core\Controller{
-
+    
     public function viewregisterAction() {
         echo self::render("register");
     } 
-     
-    public function viewindexAction() {
-        echo self::render("index");
-    }
-
+    
     public function viewloginAction() {
         echo self::render("login");
     }
 
+    public function viewindexAction() {
+        echo self::render("index");
+    }
+    
     public function viewshowAction() {
         echo self::render("show");
     }
-    
-    public function deleteAction() {
 
-    }
-    public function detailAction() {
-        
-    }
     public function registerAction() {
         $params = \Core\Request::security($_POST);
         $user = new \Model\UserModel($params);
         if (!isset($user->id))  {       
             $user->save($params);
-            self::$_render = "Votre compte a ete cree." . PHP_EOL ;
         }
-
     }
+
+    public function loginAction () {
+        $params = \Core\Request::security($_POST);
+        $user = new \Model\UserModel($params);
+        if (!isset($user->id))  {       
+            $user->login($params);
+        }
+    } 
+
     public function indexAction () {
 
     }
-    public function loginAction () {
-
-    } 
 
     public function showAction() {
 
