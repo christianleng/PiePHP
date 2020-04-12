ae7da95f9a2323e776afbeb22189373a3a4b4ae7
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:41:56 +0200

    OK

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -6,8 +6,16 @@ class UserModel extends \Core\Entity{
     public $_table;
     public $attribut;
 
+
     public function save($params) {
         return \Core\ORM::create($this->_table, $this->attribut, $params);
     }
 
+    public function login($params) {
+        return \Core\ORM::login($this->_table, $this->attribut, $params, $id);
+    }
+
+    public function read_all($params) {
+        return \Core\ORM::login($this->_table, $this->attribut, $params, $id);
+    }
 }
