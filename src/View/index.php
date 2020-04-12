4209eae4c1de6545ae4a566d5a577d2adf5c338a
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:43:39 +0200

    ok

--- src/View/index.php
+++ src/View/index.php
@@ -2,10 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MVC_PiePHP</title>
 </head>
 <body>
+    <div>
+        <a href="/MVC_PiePHP/user/viewregister"><button>Se creer un compte</button></a>
+        <a href="/MVC_PiePHP/user/viewshow"><button>Nos membres</button></a>
+        <a href="/MVC_PiePHP/user/viewlogin"><button>Se connecter</button></a>
+    </div>
+    
     <?= $view ?>
 </body>
 </html>
