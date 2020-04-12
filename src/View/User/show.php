e002cfa7bc47d4837f6bd882c3d79a5b2297cc56
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:43:04 +0200

    ok

--- src/View/User/show.php
+++ src/View/User/show.php
@@ -1,14 +1,5 @@
-<form action="/user/show" method="post">
-    <div>
-        <label for="email">email :</label>
-        <input type="email" id="email" name="email">
-    </div>
-    <div>
-        <label for="password">password :</label>
-        <input type="password" id="password" name="password">
-    </div>
-    <div>
-        <label for="submit">submit :</label>
-        <input type="submit" id="submit" name="submit">
-    </div>
-</form>
+<h1>Nos membres :</h1>
+<div>
+    <p>Email :</p>
+    <p>----------------</p>
+</div>
