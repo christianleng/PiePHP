406fb8e429029e5f0b96d05a43cc0a6df05a082a
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:42:52 +0200

    ok

--- src/View/User/register.php
+++ src/View/User/register.php
@@ -1,10 +1,10 @@
 <h2>Register</h2>
 <form action="/MVC_PiePHP/user/register" method="post">
     <div>
-        <input type="email" id="email" name="email" placeholder="Votre adresse mail">
-        <input type="email" id="email2" name="email" placeholder="Valider votre adresse mail">
-        <input type="password" id="password" name="password" placeholder="Votre mot de passe">
-        <input type="password" id="password2" name="password" placeholder="Valider votre mot de passe">
-        <input type="submit" id="submit" name="submit">
+        <input type="text" name="nom" placeholder="Votre Nom">
+        <input type="text" name="prenom" placeholder="Votre Prenom">
+        <input type="email" name="email" placeholder="Votre adresse mail">
+        <input type="password" name="password" placeholder="Votre mot de passe">
+        <input type="submit" name="submit">
     </div>
 </form>
