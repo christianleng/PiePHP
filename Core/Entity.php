64e3f686698450f422f673cde80891e74823822d
christian.leng <christian.leng@epitech.eu>
2020-04-12 12:39:33 +0200

    Entity fait

--- Core/Entity.php
+++ Core/Entity.php
@@ -3,20 +3,20 @@ namespace Core;
 
 class Entity{
     
-    public $attribut;
+    public  $attribut;
     public $reAtttribut;
-    public $_table;
+    public  $_table;
 
     public function __construct($params) {
+
         $nameClass = get_class($this);
         $this->_table = str_replace('model', '', stripslashes(strtolower($nameClass))) . "s";
-       
         if (array_key_exists('id', $params)) {
+            echo '<pre style="color: teal;">', var_dump(ORM::read($this->_table, $params)), '</pre>';
             $this->getAllAttributes(ORM::read($this->_table, $params));
         } else {
             $this->getAllAttributes($params);
         }
-
         $allAttribut = get_object_vars($this);
         $this->attribut = $allAttribut;
 
@@ -29,14 +29,19 @@ class Entity{
     }
 
     public function create($_table, $attribut, $params) {
+        echo "bonjour Entity [OK]";
         return \Core\ORM::create($this->_table, $this->attribut, $params);
     }
     
-    public function read($id) {
-        return \Core\ORM::read($this->_table, $this->attribut);
+    public function login ($_table, $attribut, $params) {
+        return \Core\ORM::login($this->_table, $this->attribut, $params);
     }
 
-    public function update($_table, $attribut, $params) {
+    public function read_all($_table, $attribut, $params) {
+        return \Core\ORM::read_all($this->_table, $this->attribut, $params);
+    }
+
+    public function update($id) {
         return \Core\ORM::update($this->_table, $this->attribut);
     }
 
